6a17d6464c1fd2a4fdf85122abcf36d6957df1c8
manaho-doi <m-doi@it-one.co.jp>
2019-12-05 16:39:28 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <label htmlFor='bar'>bar</label>
+      <input
+        tyoe='text'
+        onChange={() => {
+          console.log('I am clicked');
+        }}
+      />
     </div>
   );
 }
