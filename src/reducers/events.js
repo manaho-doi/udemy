96e3024a12932e8ecf1edaab54834577ff305815
manaho-doi <m-doi@it-one.co.jp>
2019-12-10 09:55:11 +0900

    implement eventsShow

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,8 +1,22 @@
 import _ from 'lodash';
-import { READ_EVENTS, DELETE_EVENT } from '../actions';
+import {
+  CREATE_EVENT,
+  READ_EVENTS,
+  READ_EVENT,
+  UPDATE_EVENT,
+  DELETE_EVENT
+} from '../actions';
 
 export default (events = {}, action) => {
   switch (action.type) {
+    // READ_EVENTとUPDATE_EVENTは処理が同じなので
+    // 並列で書くだけでOK
+    case READ_EVENT:
+    case CREATE_EVENT:
+    case UPDATE_EVENT:
+      const data = action.response.data;
+      console.log(action.response.data);
+      return { ...events, [data.id]: data };
     case READ_EVENTS:
       return _.mapKeys(action.response.data, 'id');
     case DELETE_EVENT:
